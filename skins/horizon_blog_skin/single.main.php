e5716a0b557d2bfae02873f8898a53a0b68c0b9c
Manuel García <solipa@gmail.com>
2015-11-18 07:22:57 +0100

    Adding sidebar to single disp

--- skins/horizon_blog_skin/single.main.php
+++ skins/horizon_blog_skin/single.main.php
@@ -108,7 +108,8 @@ siteskin_include( '_site_body_header.inc.php' );
 
 <div class="container">
 <!-- =================================== START OF MAIN AREA =================================== -->
-        <div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+		<div class="<?php echo ( $Skin->get_setting( 'layout' ) == 'single_column' ? 'col-md-12' : 'col-md-9' ); 
+			echo ( $Skin->get_setting( 'layout' ) == 'left_sidebar' ? ' pull-right' : '' ); ?>">
 
 	<?php
 	if( ! in_array( $disp, array( 'login', 'lostpassword', 'register', 'activateinfo' ) ) )
@@ -323,16 +324,91 @@ siteskin_include( '_site_body_header.inc.php' );
 		// copying the matching php file into your skin directory.
 		// ------------------------- END OF MAIN CONTENT TEMPLATE ---------------------------
 	?>
-	
+	</div>
+
 	<?php
 	if( $Skin->get_setting( 'layout' ) != 'single_column' )
 	{
 	?>
-<!-- =================================== START OF SIDEBAR =================================== -->
-		
+		<!-- =================================== START OF SIDEBAR =================================== -->
+		<aside class="col-md-3<?php echo ( $Skin->get_setting( 'layout' ) == 'left_sidebar' ? ' pull-left' : '' ); ?>">
+		<div class="evo_container evo_container__sidebar">
+		<?php
+			// ------------------------- "Sidebar" CONTAINER EMBEDDED HERE --------------------------
+			// Display container contents:
+			skin_container( NT_('Sidebar'), array(
+					// The following (optional) params will be used as defaults for widgets included in this container:
+					// This will enclose each widget in a block:
+					'block_start' => '<div class="panel panel-default evo_widget $wi_class$">',
+					'block_end' => '</div>',
+					// This will enclose the title of each widget:
+					'block_title_start' => '<div class="panel-heading"><h4 class="panel-title">',
+					'block_title_end' => '</h4></div>',
+					// This will enclose the body of each widget:
+					'block_body_start' => '<div class="panel-body">',
+					'block_body_end' => '</div>',
+					// If a widget displays a list, this will enclose that list:
+					'list_start' => '<ul>',
+					'list_end' => '</ul>',
+					// This will enclose each item in a list:
+					'item_start' => '<li>',
+					'item_end' => '</li>',
+					// This will enclose sub-lists in a list:
+					'group_start' => '<ul>',
+					'group_end' => '</ul>',
+					// This will enclose (foot)notes:
+					'notes_start' => '<div class="notes">',
+					'notes_end' => '</div>',
+					// Widget 'Search form':
+					'search_class'         => 'compact_search_form',
+					'search_input_before'  => '<div class="input-group">',
+					'search_input_after'   => '',
+					'search_submit_before' => '<span class="input-group-btn">',
+					'search_submit_after'  => '</span></div>',
+				) );
+			// ----------------------------- END OF "Sidebar" CONTAINER -----------------------------
+		?>
+		</div>
+		<div class="evo_container evo_container__sidebar2">
+		<?php
+			// ------------------------- "Sidebar" CONTAINER EMBEDDED HERE --------------------------
+			// Display container contents:
+			skin_container( NT_('Sidebar 2'), array(
+					// The following (optional) params will be used as defaults for widgets included in this container:
+					// This will enclose each widget in a block:
+					'block_start' => '<div class="panel panel-default evo_widget $wi_class$">',
+					'block_end' => '</div>',
+					// This will enclose the title of each widget:
+					'block_title_start' => '<div class="panel-heading"><h4 class="panel-title">',
+					'block_title_end' => '</h4></div>',
+					// This will enclose the body of each widget:
+					'block_body_start' => '<div class="panel-body">',
+					'block_body_end' => '</div>',
+					// If a widget displays a list, this will enclose that list:
+					'list_start' => '<ul>',
+					'list_end' => '</ul>',
+					// This will enclose each item in a list:
+					'item_start' => '<li>',
+					'item_end' => '</li>',
+					// This will enclose sub-lists in a list:
+					'group_start' => '<ul>',
+					'group_end' => '</ul>',
+					// This will enclose (foot)notes:
+					'notes_start' => '<div class="notes">',
+					'notes_end' => '</div>',
+					// Widget 'Search form':
+					'search_class'         => 'compact_search_form',
+					'search_input_before'  => '<div class="input-group">',
+					'search_input_after'   => '',
+					'search_submit_before' => '<span class="input-group-btn">',
+					'search_submit_after'  => '</span></div>',
+				) );
+			// ----------------------------- END OF "Sidebar" CONTAINER -----------------------------
+		?>
+		</div>
+	</aside>
 		
 	<?php } ?>
-	</div>
 </div><!-- ../container -->
 
 <!-- =================================== START OF FOOTER =================================== -->
